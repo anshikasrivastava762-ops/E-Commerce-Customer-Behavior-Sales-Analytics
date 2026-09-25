d956653bcb475c17b790d79eff138fa89f7b8c9c
anshikasrivastava762-ops <anshikasrivastava762@gmail.com>
2026-09-25 23:28:37 +0530

    01_data_cleaning.sql

---
+++ sql/01_data_cleaning.sql
@@ -0,0 +1,18 @@
+CREATE TABLE clean_customers AS
+SELECT 
+    customer_id,
+    age,
+    gender,
+    country
+FROM raw_customers;
+CREATE TABLE clean_transactions AS
+SELECT
+transaction_id,
+customer_id,
+DATE(transaction_date) AS transaction_date,
+product_category,
+CAST(REPLACE(unit_price, '$', '') AS DECIMAL(10,2)) AS unit_price,
+ABS(quantity) AS quantity,
+status
+FROM raw_transactions
+WHERE CAST(REPLACE(unit_price, '$', '') AS DECIMAL(10,2)) > 0;
